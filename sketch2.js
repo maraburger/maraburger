6097c96525f637e5c93d3c982c137f083187524b
Mara Burger <mara.burger01@gmail.com>
2025-07-17 17:08:18 +0200

    index2 done

--- sketch2.js
+++ sketch2.js
@@ -3,41 +3,67 @@ let sounds = [];
 let currentImage;
 
 function preload() {  
-  // Lade dein Bild "01.jpg"
+  // Lade Bilder (du kannst weitere Bilder hinzufügen, falls gewünscht)
   images.push(loadImage("bilder/01.png"));  
 
-  // Lade deine Sounds
+  // Lade Sounds
   for (let i = 1; i < 4; i++) {
     sounds.push(loadSound("sounds/" + nf(i, 2) + ".wav"));
   }
 }
 
 function setup() {  
-  createCanvas(640, 480);
+  createCanvas(windowWidth, windowHeight); // Vollflächig
   imageMode(CENTER);
-  currentImage = images[0]; // Starte mit dem geladenen Bild
+  currentImage = images[0]; // Startbild
 }
 
 function draw() {  
-  background(220);
+  background(0); // oder 220 für hell
+
   if (currentImage) {    
-    image(currentImage, width / 2, height / 2);
+    // Berechne Skalierung ohne Verzerrung (Contain)
+    let imgAspect = currentImage.width / currentImage.height;
+    let canvasAspect = width / height;
+    let w, h;
+
+    if (canvasAspect > imgAspect) {
+      // Canvas ist breiter: Höhe auf Canvas-Höhe setzen
+      h = height;
+      w = imgAspect * h;
+    } else {
+      // Canvas ist schmaler: Breite auf Canvas-Breite setzen
+      w = width;
+      h = w / imgAspect;
+    }
+
+    image(currentImage, width / 2, height / 2, w, h);
   }
 }
 
 function touchStarted() {  
-  fullscreen(true);
+  // Optional: fullscreen erst auf Touch aktivieren
+  if (!fullscreen()) {
+    fullscreen(true);
+  }
 
+  // Zufälliges Bild laden (falls du mehrere hinzufügen willst)
   let randomIndex = floor(random(images.length));
   currentImage = images[randomIndex];
 
+  // Zufälligen Sound abspielen
   let randomSoundIndex = floor(random(sounds.length));
   let selectedSound = sounds[randomSoundIndex];
 
+  // Alle Sounds stoppen, dann neuen abspielen
   for (let s of sounds) {    
     s.stop();
   }
   selectedSound.play();
 
-  return false; // Verhindert Standard-Scrolling auf Mobile
+  return false; // Verhindert Standard-Scrolling
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
